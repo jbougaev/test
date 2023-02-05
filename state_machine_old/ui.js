4f787b21b0019b67b23cae3a3099bbd890515218
shoshiko <13385068+shoshiko@users.noreply.github.com>
2023-02-05 10:56:10 -0500

    refactoring

--- state_machine_old/ui.js
+++ state_machine_old/ui.js
@@ -11,5 +11,12 @@ export class UI {
         this.ctx.textAlign = 'left';
         this.ctx.fillStyle = 'black';
         this.ctx.fillText('Score: ' + this.game.score, 20, 50);
+
+        this.ctx.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
+        this.ctx.fillText('Time: ' + this.game.time, 20, 100);
+        if (this.game.gameOver) {
+            this.ctx.font = this.fontSize * 2 + 'px ' + this.fontFamily;
+            this.ctx.fillText('Game is over!', this.game.gameWidth * 0.4, this.game.gameHeight * 0.4);
+        }
     }
 }
